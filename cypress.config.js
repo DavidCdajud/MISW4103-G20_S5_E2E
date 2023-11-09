dca4443a790cb5362a0913861e012f74c2dcbd86
Javier Fajardo <jd.fajardor1@uniandes.edu.co>
2023-11-08 19:27:27 -0500

    creacion de proyecto base con el admin de ghost.

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+   // video: true,
+    specPattern: "cypress/e2e/**/*.{cy,spec}.{js,ts}",
+    baseUrl: 'http://localhost:2368',
+    supportFile: 'cypress/support/commands.js',
+    videosFolder: 'cypress/videos',
+    screenshotsFolder: 'cypress/screenshots'
+  },
+})
---
+++ cypress/support/commands.js
@@ -0,0 +1,34 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('loginToGhost', () => {
+    cy.visit('/ghost/#/signin');
+
+    cy.get('input[name="identification"]').type(Cypress.env('EMAIL'));
+    cy.get('input[name="password"]').type(Cypress.env('PASSWORD'), { log: false });
+
+    cy.get('button[type="submit"]').click();
+});
