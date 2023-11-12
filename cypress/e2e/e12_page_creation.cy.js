1606af50e0d32fda60170689c1d7e7570c980f41
Javier Fajardo <jd.fajardor1@uniandes.edu.co>
2023-11-12 16:53:36 -0500

    feat(test) adicion de screenshots.

--- cypress/e2e/e12_page_creation.cy.js
+++ cypress/e2e/e12_page_creation.cy.js
@@ -3,6 +3,7 @@ describe('Page Creation and Publishing in Ghost', () => {
     const pageTitle = 'Título de Nueva Página';
     const publishButton = 'Publish';
     const successMessage = 'Ready, set, publish. Share it with the world.';
+    const caseFolder = 'caso12';
 
     before(() => {
         cy.loginToGhost();
@@ -12,8 +13,10 @@ describe('Page Creation and Publishing in Ghost', () => {
         cy.visit('/ghost/#/pages');
         cy.contains(newPageButton).click();
         cy.url().should('include', '/editor/page');
-        cy.get('textarea[placeholder="Page title"]').type(pageTitle);
-        cy.contains('button', 'Publish').click();
-        cy.contains('Ready, set, publish. Share it with the world.').should('be.visible');
+        cy.get('textarea[placeholder="Page title"]').type(pageTitle).type('{enter}');
+        cy.screenshot(`${caseFolder}/after-typing-title`);
+        cy.contains(publishButton).click();
+        cy.contains(successMessage).should('be.visible');
+        cy.screenshot(`${caseFolder}/after-publish-success`);
     });
 });
