6cbdcda6016ac92f64dc81f33b5a46f8cc6fcb87
DavidCdajud <142352460+DavidCdajud@users.noreply.github.com>
2023-11-15 11:57:56 -0500

    Config env

--- cypress.config.js
+++ cypress.config.js
@@ -2,16 +2,16 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-   // video: true,
+    // video: true,
     specPattern: "cypress/e2e/**/*.{cy,spec}.{js,ts}",
-    baseUrl: 'http://localhost:2368',
+    baseUrl: 'http://localhost:3001/ghost/',
     supportFile: 'cypress/support/commands.js',
     videosFolder: 'cypress/videos',
     screenshotsFolder: 'cypress/screenshots',
     viewportWidth: 1280,
     viewportHeight: 800
   },
-  env:{
+  env: {
     OldPass: "abcde12345",
     NewPass: "Test123456"
   }
---
+++ cypress/e2e/v3.42/e18_XCard.cy.js
@@ -0,0 +1,44 @@
+import 'cypress-file-upload';
+
+describe('X Card', function () {
+    it('Customize structured data of your site for X', function () {
+        cy.viewport(1900, 1500)
+        cy.visit('http://localhost:3001/ghost/');
+        inciarSesion();
+        cy.wait(2000);
+        settingsXCard();
+    })
+})
+
+function inciarSesion() {
+    cy.get('#identification').type('pruebas@correo.com');
+    cy.wait(1000);
+    cy.get('#password').type('abcde12345');
+    cy.wait(1000);
+    cy.get('#ember5').click();
+
+}
+
+function settingsXCard() {
+    cy.visit(Cypress.e2e(baseUrl) + 3001);
+    cy.get('[data-test-nav="settings"]').click();
+    cy.wait(1000);
+    cy.get('div.flex.flex-col input[type="text"][placeholder="Search"]').click().type('X');
+    cy.wait(1000);
+    cy.get('[data-testid="twitter"]').contains('Edit').click();
+    cy.wait(1000);
+    cy.screenshot('/caso18/Pre-XCard');
+    cy.get('label[for="twitter-image"] input[type="file"]').attachFile('descarga.png');
+    cy.wait(1000);
+    cy.get('input[placeholder="Pruebas automatizadas"]').type('My first E2E Card');
+    cy.wait(1000);
+    cy.get('input[placeholder="Thoughts, stories and ideas."]').type('lorem ipsum dolor sit amet');
+    cy.wait(1000);
+    cy.screenshot('/caso18/Post-XCard');
+    cy.get('[data-testid="twitter"]').contains('Save').click();
+    cy.wait(2000);
+    cy.screenshot('/caso18/Saved-XCard');
+}
+
+
+
