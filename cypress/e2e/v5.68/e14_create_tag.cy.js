812951f07cb97a36a65830ab794cef9025ae7c1d
Javier Fajardo <jd.fajardor1@uniandes.edu.co>
2023-11-16 20:22:53 -0500

    ajuste de script de para captura de screenshot para la version de ghost 5.68

--- cypress/e2e/v5.68/e14_create_tag.cy.js
+++ cypress/e2e/v5.68/e14_create_tag.cy.js
@@ -2,7 +2,8 @@ describe('Create a tag in Ghost Admin', () => {
     const tagName = 'New Tag Name';
     const tagDescription = 'A description for the new tag';
     const tagsUrl = '/ghost/#/tags';
-    const caseFolder = 'caso14';
+    const versionFolder = Cypress.config('baseFolder568');
+    const caseFolder = `${versionFolder}caso14`;
 
     beforeEach(() => {
         cy.loginToGhost();
@@ -10,14 +11,15 @@ describe('Create a tag in Ghost Admin', () => {
 
     it('should create a new tag', () => {
         cy.visit(tagsUrl);
+        cy.screenshot(`${caseFolder}/01-after-visiting-tags`);
         cy.contains('New tag').click();
-        cy.screenshot(`${caseFolder}/before-creating-tag`);
+        cy.screenshot(`${caseFolder}/02-before-creating-tag`);
         cy.get('input[name="name"]').type(tagName);
         cy.get('textarea[name="description"]').type(tagDescription);
-        cy.screenshot(`${caseFolder}/after-entering-tag-info`);
+        cy.screenshot(`${caseFolder}/03-after-entering-tag-info`);
         cy.contains('Save').click();
-        cy.screenshot(`${caseFolder}/after-saving-tag`);
+        cy.screenshot(`${caseFolder}/04-after-saving-tag`);
         cy.contains(tagName).should('exist');
-        cy.screenshot(`${caseFolder}/after-tag-created`);
+        cy.screenshot(`${caseFolder}/05-after-tag-created`);
     });
 });
