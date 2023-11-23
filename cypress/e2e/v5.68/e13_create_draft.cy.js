9385f675362e660954f819b4ddd700785a00092a
Javier Fajardo <jd.fajardor1@uniandes.edu.co>
2023-11-22 19:28:58 -0500

    adicion de base de faker

--- cypress/e2e/v5.68/e13_create_draft.cy.js
+++ cypress/e2e/v5.68/e13_create_draft.cy.js
@@ -8,20 +8,35 @@ describe('Create a draft in Ghost', () => {
     const caseFolder = `${versionFolder}caso13`;
 
     beforeEach(() => {
+        cy.clearCookies();
+        cy.clearLocalStorage();
         cy.loginToGhost(baseUrl);
     });
 
     it('should create a draft post', () => {
         cy.visit('/ghost/#/editor/post');
         cy.url().should('include', '/ghost/#/editor/post');
-        cy.screenshot(`${caseFolder}/1-after-navigating-to-post-creation`);
+        cy.conditionalScreenshot(`${caseFolder}/1-after-navigating-to-post-creation`);
         cy.get('textarea[placeholder="Post title"]', { timeout: 10000 }).should('be.visible').type(postTitle);
-        cy.screenshot(`${caseFolder}/2-after-typing-title`);
+        cy.conditionalScreenshot(`${caseFolder}/2-after-typing-title`);
         cy.xpath('//p[@data-koenig-dnd-droppable="true"]').first().click({force: true}).type(postContent);
-        cy.screenshot(`${caseFolder}/3-after-typing-content`);
+        cy.conditionalScreenshot(`${caseFolder}/3-after-typing-content`);
         cy.xpath(publishButtonXpath).click();
-        cy.screenshot(`${caseFolder}/4-after-clicking-publish`);
+        cy.conditionalScreenshot(`${caseFolder}/4-after-clicking-publish`);
         cy.contains(successMessage).should('be.visible');
-        cy.screenshot(`${caseFolder}/5-after-publish-success`);
+        cy.conditionalScreenshot(`${caseFolder}/5-after-publish-success`);
     });
+
+    it('should create a draft post sin descripcion', () => {
+        cy.visit('/ghost/#/editor/post');
+        cy.url().should('include', '/ghost/#/editor/post');
+        cy.conditionalScreenshot(`${caseFolder}/1`);
+        cy.get('textarea[placeholder="Post title"]', { timeout: 10000 }).should('be.visible').type(postTitle);
+        cy.conditionalScreenshot(`${caseFolder}/2`);
+        cy.xpath(publishButtonXpath).click();
+        cy.conditionalScreenshot(`${caseFolder}/4`);
+        cy.contains(successMessage).should('be.visible');
+        cy.conditionalScreenshot(`${caseFolder}/5`);
+    });
+
 });
