6ca4f9d8b126c71d983bd697948b41200b381adb
jhosalarcon <ja.alarcon2@uniandes.edu.co>
2023-11-25 20:43:51 -0500

    :sparkles: Add test cases

---
+++ cypress/e2e/v5.68/e4_code_injection_dinamico.cy.js
@@ -0,0 +1,63 @@
+const dataMocka = require('../../fixtures/e16_e20-data.json');
+
+let varTitle = new String();
+let varDescription = new String();
+
+varTitle = dataMocka[getRandomArbitrary(1, 10)].dimTitle;
+varDescription = dataMocka[getRandomArbitrary(1, 50)].dimDescription;
+
+describe('Gestionar code injection', function() {
+  it('Agregar y verificar code injection de ghost', function() {
+     
+      cy.visit('http://localhost:2368/ghost/');
+      iniciarSesion(Cypress.env('OldPass'));
+      cy.wait(2000);
+      ingresarSettingsCodeInjection();
+      writeText();
+      cy.visit('http://localhost:2368/');
+      validarHeaderFooter();
+      limpiarHeaderFooter();
+  })
+})
+
+function iniciarSesion(pass){
+  cy.get('#identification').type('pruebas@correo.com');
+  cy.wait(1000);
+  cy.get('#password').type(pass);
+  cy.wait(1000);
+  cy.screenshot('/v5.68/dinamico/caso4/1-inicioSesion');
+  cy.get('#ember5').click();
+}
+
+function ingresarSettingsCodeInjection(){
+  cy.get('#ember34').click();
+  cy.wait(1000);
+  cy.screenshot('/v5.68/dinamico/caso4/2-setting');
+  cy.contains('a', 'Code injection').click();
+}
+
+function writeText(){
+  cy.get("#ghost-head").type(varTitle);
+  cy.get("#ghost-foot").type(varDescription);
+  cy.wait(2000);
+  cy.screenshot('/v5.68/dinamico/caso4/3-write');
+  cy.contains('span','Save').click();
+} 
+
+function validarHeaderFooter(){
+  cy.contains('body', varTitle).should('be.visible');
+  cy.contains('body', varDescription).should('be.visible');
+  cy.wait(2000);
+  cy.screenshot('/v5.68/dinamico/caso4/4-validate');
+}
+
+function limpiarHeaderFooter(){
+  cy.visit('http://localhost:2368/ghost/');
+  cy.get('#ember24').click();
+  cy.contains('a', 'Code injection').click();
+  cy.get("#ghost-head").click().type('{selectAll}{del}');
+  cy.wait(2000);
+  cy.get("#ghost-foot").click().type('{selectAll}{del}');
+  cy.wait(2000);
+  cy.contains('span','Save').click();
+} 
---
+++ cypress/e2e/v5.68/e5_ announcement_bar_apriori.cy.js
@@ -0,0 +1,53 @@
+const word = "$#%#$$@#$@!!*(&";
+describe('Gestionar code injection', function() {
+  it('Agregar y verificar code injection de ghost', function() {
+      cy.visit('http://localhost:2368/ghost/');
+      iniciarSesion(Cypress.env('OldPass'));
+      cy.wait(2000);
+      ingresarSettingsAnnouncement();
+      writeText();
+      cy.visit('http://localhost:2368/');
+      validarAnnouncementBar();
+      limpiarAnnouncementBar();
+  })
+})
+
+function iniciarSesion(pass){
+  cy.get('#identification').type('pruebas@correo.com');
+  cy.wait(1000);
+  cy.get('#password').type(pass);
+  cy.wait(1000);
+  cy.screenshot('/v5.68/caso5/apriori/1-inicioSesion');
+  cy.get('#ember5').click();
+}
+
+function ingresarSettingsAnnouncement(){
+  cy.get('#ember34').click();
+  cy.wait(1000);
+  cy.screenshot('/v5.68/caso5/apriori/2-setting');
+  cy.contains('a', 'Announcement bar').click();
+}
+
+function writeText(){
+  cy.get("p[data-koenig-dnd-droppable='true']").type(word);
+  cy.wait(2000);
+  cy.screenshot('/v5.68/caso5/apriori/3-write');
+  cy.wait(2000);
+  cy.contains('span','Save').click();
+} 
+
+function validarAnnouncementBar(){
+  cy.contains('span', 'TestAnnouncement').should('be.visible');
+  cy.wait(2000);
+  cy.screenshot('/v5.68/caso5/apriori/4-validate');
+}
+
+function limpiarAnnouncementBar(){
+  cy.visit('http://localhost:2368/ghost/');
+  cy.get('#ember24').click();
+  cy.contains('a', 'Announcement bar').click();
+  cy.get("p[data-koenig-dnd-droppable='true']").click().type('{selectAll}{del}');
+  cy.contains('span','Save').click();
+  cy.wait(2000);
+  cy.visit('http://localhost:2368/ghost/');
+}
